ad4cecb3fd3e7134a352ceee1bd59aae17ad274d
matveev96 <sasha.matveev1996@icloud.com>
2024-08-05 20:51:19 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+  const arr = str.split('');
+  arr.reverse();
+  const newStr = arr.join('');
+  return parseInt(newStr);
 }
